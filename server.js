63de74badfa982d9c65ae2b35d306285f0e92700
songzhanpeng <songzhanpeng@iauto.com>
2024-04-28 10:49:54 +0800

    feat: 修改

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const app = express();
 const PORT = process.env.PORT || 3000; // 你可以根据需要设置端口号
 const { getSixs } = require("./services/index");
 
-// 设置路由和中间件
-// 例如：
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
 app.get("/", async (req, res) => {
   try {
     res.send("hello world");
@@ -18,13 +12,13 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/getSixs", async (req, res) => {
-    try {
-      const { data } = await getSixs()
-      res.json(data);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to fetch data from external API" });
-    }
-  });
+  try {
+    const { data } = await getSixs();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data from external API" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
